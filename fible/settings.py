6f467e470523077191727099b9dc55c35e725fa2
robi3377 <robert.uibar@gmail.com>
2022-07-31 18:25:58 +0300

    deployment changes

--- fible/settings.py
+++ fible/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k!!g8rz)k)-an8a)z+c%8+e+gbxs5c9v-#w-&wa)t%&mxz$5&c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'fible3proj.herokuapp.com']
 
 
 # Application definition
